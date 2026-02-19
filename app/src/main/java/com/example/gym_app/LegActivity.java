06d39bfc00a668d39a6b9a52b2ea3997df13c2da
BjMei <bjoern-meinhardt@gmx.de>
2026-02-19 12:40:27 +0100

    Clear Statistik und Fortschritt Inhalte

--- app/src/main/java/com/example/gym_app/LegActivity.java
+++ app/src/main/java/com/example/gym_app/LegActivity.java
@@ -13,6 +13,7 @@ import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
