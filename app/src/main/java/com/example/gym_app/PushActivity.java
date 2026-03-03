637b643cdf74f8ff21a9341f8c2d2d02017a497c
BjMei <bjoern-meinhardt@gmx.de>
2026-03-03 23:33:40 +0100

    Add app namespace back to workout layout root

--- app/src/main/java/com/example/gym_app/PushActivity.java
+++ app/src/main/java/com/example/gym_app/PushActivity.java
@@ -685,7 +685,7 @@ public class PushActivity extends AppCompatActivity {
 
         EditText etSeatPositions = dialog.findViewById(R.id.etSeatPositions);
         Button btnCancel = dialog.findViewById(R.id.btnCancel);
-        ImageButton btnSaveSettings = dialog.findViewById(R.id.btnSaveSettings);
+        Button btnSaveSettings = dialog.findViewById(R.id.btnSaveSettings);
 
         // Gespeicherte Einstellungen laden
         String savedPositions = sharedPreferences.getString(selectedExercise, "");
